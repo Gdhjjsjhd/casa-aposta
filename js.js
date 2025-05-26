40afd5e32a889a195046e90267789f76196f377e
Gabriel <160670197+Gdhjjsjhd@users.noreply.github.com>
2025-05-26 20:15:49 -0300

    Add files via upload

--- js.js
+++ js.js
@@ -69,7 +69,56 @@ rl.question('Digite o nome do time 1: ', (time1) => {
     rl.question('Digite o valor que você deseja depositar: ', (valorDeposito) => {
         saldo += parseFloat(valorDeposito);
         console.log(`Seu novo saldo é: R$ ${saldo}`);
-        rl.close();
+        rl.question('Digite o nome do time 1: ', (time1) => {
+    rl.question('Digite o nome do time 2: ', (time2) => {
+        const odiTime1 = gerarOdiTime1();
+        const odiTime2 = gerarOdiTime2();
+
+        console.log(`A odd do time ${time1} é: ${odiTime1}`);
+        console.log(`A odd do time ${time2} é: ${odiTime2}`);
+
+        rl.question(`Digite para qual time você quer apostar\n 1 - ${time1}\n 2 - ${time2}\n ` ,(timedaAposta) => {
+            if(timedaAposta === '1'){
+                rl.question(`Digite o valor da aposta para o time ${time1}: `, (valorAposta) => {
+                   if(valorAposta <= saldo){
+                    var retorno = (odiTime1 * valorAposta)
+       rl.question (`Qual time ganhou?\n  1 - ${time1}\n  2 - ${time2}\n `, (timeGanhador) => {
+            if(timeGanhador === timedaAposta){
+                    saldo += retorno;
+                    console.log(`Parabéns, você ganhou! Seu novo saldo é: R$ ${saldo}`);
+                    rl.close();
+                }else{
+                    saldo -= valorAposta;
+                    console.log(`Que pena, você perdeu! Seu novo saldo é: R$ ${saldo}`);
+                    rl.close();
+                }
+});  
+                    
+                    }
+                    
+                });
+            }else if(timedaAposta === '2'){
+                rl.question(`Digite o valor da aposta para o time ${time2}: `, (valorAposta) => {
+                    if(valorAposta <= saldo){
+                        var retorno = (odiTime2 * valorAposta)
+       rl.question (`Qual time ganhou?\n  1 - ${time1}\n  2 - ${time2}\n `, (timeGanhador) => {
+            if(timeGanhador === timedaAposta){
+                    saldo += retorno;
+                    console.log(`Parabéns, você ganhou! Seu novo saldo é: R$ ${saldo}`);
+                    rl.close();
+                }else{
+                    saldo -= valorAposta;
+                    console.log(`Que pena, você perdeu! Seu novo saldo é: R$ ${saldo}`);
+                    rl.close();
+                }
+});  
+                    }
+                });
+        }
+   
+});
+});
+})        
     });
 }
 })
